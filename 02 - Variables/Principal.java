4dd01cfc11622b1acb7e1cf118ef287952943338
Miguel-Alzate <miguelangel4817@gmail.com>
2023-05-12 17:39:51 -0500

    Variables y Primer ejercicio

---
+++ 02 - Variables/Principal.java
@@ -0,0 +1,24 @@
+public class Principal{
+    public static void main(String[] args){
+
+        byte turno = 15;
+        short loteria_risaralda = 1247;
+        int saldo_bancario = 1500000;
+        long tarjeta_identidad = 20000000*5;
+        float nota = 3.5f;
+        double pi = 3.141592653;
+        boolean bandera = true;
+        char letra = 263;
+        String nombres = "Miguel Alzate";
+
+        System.out.println("Turno: "+turno);
+        System.out.println("Loteria Risaralda: "+loteria_risaralda);
+        System.out.println("Saldo Bancario: "+saldo_bancario);
+        System.out.println("Tarjeta de Identidad: "+tarjeta_identidad);
+        System.out.println("Nota: "+nota);
+        System.out.println("Pi: "+pi);
+        System.out.println("Banderita: "+bandera);
+        System.out.println("Letra: "+letra);
+        System.out.println("Nombre: "+nombres);
+    }
+}
