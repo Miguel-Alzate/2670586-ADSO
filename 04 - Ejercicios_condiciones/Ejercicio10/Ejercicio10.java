cc68efdf65f49328a33b2a8e08656e1727c98ac9
Miguel-Alzate <miguelangel4817@gmail.com>
2023-05-28 20:22:04 -0500

    Ejercicios 1 al 10 completos

--- 04 - Ejercicios_condiciones/Ejercicio10/Ejercicio10.java
+++ 04 - Ejercicios_condiciones/Ejercicio10/Ejercicio10.java
@@ -9,6 +9,12 @@ Scanner entrada = new Scanner(System.in);
     float nota_4 = 0;
     float nota_5 = 0;
     float promedio = 0;
+    float nota_aprobar = 0;
+    float nota_necesita1 = 0;
+    float nota_necesita2 = 0;
+    float nota_necesita3 = 0;
+    float nota_necesita4 = 0;
+    float nota_necesita5 = 0;
 
     System.out.print("Ingresa la nota para Matematicas: ");
     nota_1 = entrada.nextFloat();
@@ -22,6 +28,7 @@ Scanner entrada = new Scanner(System.in);
     nota_5 = entrada.nextFloat();
 
     promedio = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5)/5;
+
 
     System.out.println("         ");
     System.out.println("Tu nota de Matematicas es: "+nota_1);
@@ -72,19 +79,28 @@ Scanner entrada = new Scanner(System.in);
         System.out.println("Asignatura con peor nota: Deportes");
     }
 
+    nota_aprobar = 15 - (promedio*5);
+    nota_necesita1 = (nota_aprobar / 1)+nota_1;
+    nota_necesita2 = (nota_aprobar / 1)+nota_2;
+    nota_necesita3 = (nota_aprobar / 1)+nota_3;
+    nota_necesita4 = (nota_aprobar / 1)+nota_4;
+    nota_necesita5 = (nota_aprobar / 1)+nota_5;
+    
+
+
 
     System.out.println("Promedio de notas: "+promedio);
 
     if(promedio <3 & nota_1 < nota_2 & nota_1 < nota_3 & nota_1 < nota_4 & nota_1 < nota_5){
-        System.out.println("Debio obtener una nota de:--- en Matematicas para aprobar el periodo");
+        System.out.println("Debio obtener una nota de: "+nota_necesita1+"  en Matematicas para aprobar el periodo");
     }else if(promedio <3 & nota_2 < nota_1 & nota_2 < nota_3 & nota_2 < nota_4 & nota_2 < nota_5){
-        System.out.println("Debio obtener una nota de:--- en Español aprobar el periodo");
+        System.out.println("Debio obtener una nota de: "+nota_necesita2+" en Español aprobar el periodo");
     }else if(promedio <3 & nota_3 < nota_1 & nota_3 < nota_2 & nota_3 < nota_4 & nota_3 < nota_5){
-        System.out.println("Debio obtener una nota de:--- en Inglés aprobar el periodo");
+        System.out.println("Debio obtener una nota de: "+nota_necesita3+" en Inglés aprobar el periodo");
     }else if(promedio <3 & nota_4 < nota_1 & nota_4 < nota_2 & nota_4 < nota_3 & nota_4 < nota_5){
-        System.out.println("Debio obtener una nota de:--- en Ciencias Naturales aprobar el periodo");
+        System.out.println("Debio obtener una nota de: "+nota_necesita4+" en Ciencias Naturales aprobar el periodo");
     }else if(promedio <3 & nota_5 < nota_1 & nota_5 < nota_2 & nota_5 < nota_3 & nota_5 < nota_4){
-        System.out.println("Debio obtener una nota de:--- en Deportes aprobar el periodo");
+        System.out.println("Debio obtener una nota de: "+nota_necesita5+" en Deportes aprobar el periodo");
     }
     
    
