674a8a5a74acb5ad230318c756fd667705c5a272
Andres <ag016498@gmail.com>
2023-06-09 17:12:55 -0500

    Ejericios ciclos (1 y 2)

---
+++ 06 - Ejercicios_ciclos/Ejercicio01/Ejercicio01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ejercicio01{
+    public static void main (String [] args){
+    Scanner entrada = new Scanner(System.in);
+
+    int cantidad_notas = 0;
+    double suma_notas = 0;
+    double notas = 0;
+    int contador = 1;
+    
+    System.out.print("Señor usuario, ingrese la cantidad de notas: ");
+    cantidad_notas = entrada.nextInt();
+
+		while( contador <= cantidad_notas ){
+		 
+		 	System.out.print("Ingrese la nota "+contador+" = ");
+            notas = entrada.nextDouble();
+			contador = contador + 1;
+            suma_notas = suma_notas + notas;
+		}
+        double promedio = suma_notas / cantidad_notas ;
+        System.out.println("Este es el promedio de las notas: "+ promedio);
+
+
+    }
+}
---
+++ 06 - Ejercicios_ciclos/Ejercicio02/Ejercicio02.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Ejercicio02{
+    public static void main (String [] args){
+    Scanner entrada = new Scanner(System.in);
+
+    int cantidad_notas = 0;
+    double suma_notas = 0;
+    double notas = 0;
+    int contador = 1;
+    int creditos = 0;
+    int suma_creditos = 0;
+    double suma_nota_por_creditos = 0;
+    
+    System.out.print("Señor usuario, ingrese la cantidad de Materias: ");
+    cantidad_notas = entrada.nextInt();
+
+		while( contador <= cantidad_notas ){
+		 
+		 	System.out.print("Ingrese la nota del Materia "+contador+" = ");
+            notas = entrada.nextDouble();
+
+            System.out.print("Ingrese el crédito de la Materia "+contador+" = ");
+            creditos = entrada.nextInt();
+
+			contador = contador + 1;
+            suma_notas = suma_notas + notas;
+            suma_creditos = suma_creditos + creditos;
+            suma_nota_por_creditos = suma_nota_por_creditos + (notas * creditos);
+
+		}
+
+        double promedio = suma_nota_por_creditos / suma_creditos ;
+        System.out.println("Este es el total de creditos: "+ suma_creditos);
+        System.out.println("Este es el promedio de las notas: "+ promedio);
+
+
+    }
+}
