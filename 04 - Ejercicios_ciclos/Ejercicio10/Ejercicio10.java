fbcfaaf9a8579887f36676079c8c70a466a7a7ea
Miguel-Alzate <miguelangel4817@gmail.com>
2023-06-25 17:31:04 -0500

    Ejercicios 1 al 10 Terminados

--- 04 - Ejercicios_ciclos/Ejercicio10/Ejercicio10.java
+++ 04 - Ejercicios_ciclos/Ejercicio10/Ejercicio10.java
@@ -1,13 +1,40 @@
 import java.util.Scanner;
 public class Ejercicio10{
-    public static void main (String [] args){
-    Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingresar numero: ");
+        int num = teclado.nextInt();
+
+        if(num < 1 || num > 11){
+            System.out.println("El numero no se encuentra en el rango permitido");
+        }else{
+            System.out.println("Los numeros primos encontrados son:");
+
+            int primo = 2;
+            int contador = 0;
 
 
+            while(contador < (num*num)){
+                int divisores = 0;
+                for(int i = 1; i<=primo ; i++){
+                    if(primo % i == 0){
+                        divisores = divisores + 1;
+                    }
+                }
+                if(divisores == 2){
+                    System.out.print(" "+"["+primo+"]");
+                    primo++;
+                    contador++;
+                    if(contador % num == 0){
+                        System.out.println("");
+                    }
+                }else{
+                    primo++; 
+                }
 
-
-
-
-
+            }
+        }            
     }
-}
+}    
