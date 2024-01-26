406af0f558db33ede3506a1a050f239bd432057f
John0w0 <cesarcarrig8@gmail.com>
2024-01-26 16:34:05 -0500

    Avances netbeans

---
+++ 18 - POO - UI - NETBEANS/src/principal/AppRegistro.java
@@ -0,0 +1,15 @@
+
+package principal;
+
+
+public class AppRegistro {
+
+   
+    public static void main(String[] args) {
+        
+        Presentacion ventana = new Presentacion();
+        PresentacionV2 ventana02 = new PresentacionV2();
+        
+    }
+    
+}
