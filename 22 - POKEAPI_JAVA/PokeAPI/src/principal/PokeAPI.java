5c600bee9aa1faa50e80d1a44e442b33cd1f8905
Miguel-Alzate <miguelangel4817@gmail.com>
2024-05-24 16:07:06 -0500

    Carga de imagenes completa, falta paginador

--- 22 - POKEAPI_JAVA/PokeAPI/src/principal/PokeAPI.java
+++ 22 - POKEAPI_JAVA/PokeAPI/src/principal/PokeAPI.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.io.IOException;
 import javax.swing.ImageIcon;
 
 public class PokeAPI extends javax.swing.JFrame {
@@ -23,6 +24,8 @@ public class PokeAPI extends javax.swing.JFrame {
     String texto = "Numero de página: ";
     String aux;
     int pagina_temp = 1;
+    JsonObject URL_actual_pokemon;
+    int contador_imagenes = 1;
   
     public PokeAPI() {
         initComponents();
@@ -108,7 +111,6 @@ public class PokeAPI extends javax.swing.JFrame {
 
         etq_nombre_pokemon.setBackground(new java.awt.Color(255, 255, 255));
         etq_nombre_pokemon.setFont(new java.awt.Font("Arial", 1, 31)); // NOI18N
-        etq_nombre_pokemon.setForeground(new java.awt.Color(0, 0, 0));
         etq_nombre_pokemon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         etq_nombre_pokemon.setText("Hola!");
 
@@ -123,15 +125,12 @@ public class PokeAPI extends javax.swing.JFrame {
         jScrollPane2.setViewportView(lista_habilidades);
 
         etq_img_pokemon.setBackground(new java.awt.Color(255, 255, 255));
-        etq_img_pokemon.setForeground(new java.awt.Color(0, 0, 0));
         etq_img_pokemon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         etq_img_pokemon.setText("the game");
 
         panel_paginador.setBackground(new java.awt.Color(204, 204, 204));
 
-        btn_atras_total.setBackground(new java.awt.Color(255, 255, 255));
         btn_atras_total.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        btn_atras_total.setForeground(new java.awt.Color(0, 0, 0));
         btn_atras_total.setText("<<");
         btn_atras_total.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -139,9 +138,7 @@ public class PokeAPI extends javax.swing.JFrame {
             }
         });
 
-        btn_adelante_total.setBackground(new java.awt.Color(255, 255, 255));
         btn_adelante_total.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        btn_adelante_total.setForeground(new java.awt.Color(0, 0, 0));
         btn_adelante_total.setText(">>");
         btn_adelante_total.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -149,9 +146,7 @@ public class PokeAPI extends javax.swing.JFrame {
             }
         });
 
-        btn_atras.setBackground(new java.awt.Color(255, 255, 255));
         btn_atras.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        btn_atras.setForeground(new java.awt.Color(0, 0, 0));
         btn_atras.setText("<");
         btn_atras.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -159,9 +154,7 @@ public class PokeAPI extends javax.swing.JFrame {
             }
         });
 
-        btn_adelante.setBackground(new java.awt.Color(255, 255, 255));
         btn_adelante.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        btn_adelante.setForeground(new java.awt.Color(0, 0, 0));
         btn_adelante.setText(">");
         btn_adelante.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -171,7 +164,6 @@ public class PokeAPI extends javax.swing.JFrame {
 
         etq_num_pagina.setBackground(new java.awt.Color(255, 255, 255));
         etq_num_pagina.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        etq_num_pagina.setForeground(new java.awt.Color(0, 0, 0));
 
         javax.swing.GroupLayout panel_paginadorLayout = new javax.swing.GroupLayout(panel_paginador);
         panel_paginador.setLayout(panel_paginadorLayout);
@@ -202,6 +194,18 @@ public class PokeAPI extends javax.swing.JFrame {
                     .addComponent(etq_num_pagina))
                 .addGap(14, 14, 14))
         );
+
+        etq_atras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                etq_atrasActionPerformed(evt);
+            }
+        });
+
+        etq_adelante.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                etq_adelanteActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -277,6 +281,103 @@ public class PokeAPI extends javax.swing.JFrame {
     private void btn_adelante_totalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_adelante_totalActionPerformed
         cambiarPagina(1);
     }//GEN-LAST:event_btn_adelante_totalActionPerformed
+
+    private void etq_atrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_etq_atrasActionPerformed
+        contador_imagenes --;
+        if(contador_imagenes == 1){
+            try {
+                JsonObject sprites = URL_actual_pokemon.getAsJsonObject("sprites");
+                String imgUrl = sprites.get("other").getAsJsonObject()
+                                .getAsJsonObject("official-artwork")
+                                .get("front_default").getAsString();
+
+                Image image = ImageIO.read(new URL(imgUrl));
+
+                Image resizedImage = image.getScaledInstance(272, 254, Image.SCALE_SMOOTH);
+                etq_img_pokemon.setIcon(new ImageIcon(resizedImage));
+                etq_img_pokemon.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(contador_imagenes == 2){
+            
+            System.out.println("url:" +URL_actual_pokemon);
+            try {
+                JsonObject sprites = URL_actual_pokemon.getAsJsonObject("sprites");
+                  String imgUrl = sprites.get("other").getAsJsonObject()
+                                .getAsJsonObject("official-artwork")
+                                .get("front_shiny").getAsString();
+
+                Image image = ImageIO.read(new URL(imgUrl));
+
+                Image resizedImage = image.getScaledInstance(260, 245, Image.SCALE_SMOOTH);
+                etq_img_pokemon.setIcon(new ImageIcon(resizedImage));
+                etq_img_pokemon.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+         
+        if(contador_imagenes == 1){
+            etq_atras.setEnabled(false);
+        }
+        
+        if(contador_imagenes != 1){
+            etq_atras.setEnabled(true);
+        }
+        
+        etq_adelante.setEnabled(true);
+        
+    }//GEN-LAST:event_etq_atrasActionPerformed
+
+    private void etq_adelanteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_etq_adelanteActionPerformed
+        contador_imagenes ++;
+        if(contador_imagenes == 2){
+            try {
+                JsonObject sprites = URL_actual_pokemon.getAsJsonObject("sprites");
+                String imgUrl = sprites.get("other").getAsJsonObject()
+                                .getAsJsonObject("official-artwork")
+                                .get("front_shiny").getAsString();
+
+                Image image = ImageIO.read(new URL(imgUrl));
+
+                Image resizedImage = image.getScaledInstance(272, 254, Image.SCALE_SMOOTH);
+                etq_img_pokemon.setIcon(new ImageIcon(resizedImage));
+                etq_img_pokemon.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(contador_imagenes == 3){
+            
+            System.out.println("url:" +URL_actual_pokemon);
+            try {
+                JsonObject sprites = URL_actual_pokemon.getAsJsonObject("sprites");
+                String imgUrl = sprites.get("other").getAsJsonObject()
+                                .getAsJsonObject("home")
+                                .get("front_default").getAsString();
+
+                Image image = ImageIO.read(new URL(imgUrl));
+
+                Image resizedImage = image.getScaledInstance(260, 245, Image.SCALE_SMOOTH);
+                etq_img_pokemon.setIcon(new ImageIcon(resizedImage));
+                etq_img_pokemon.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+         
+        if(contador_imagenes == 3){
+            etq_adelante.setEnabled(false);
+        }
+        
+        if(contador_imagenes != 3){
+            etq_adelante.setEnabled(true);
+        }
+        
+        etq_atras.setEnabled(true);
+    }//GEN-LAST:event_etq_adelanteActionPerformed
     
     public void cambiarPagina(int numero) {
     String texto = "Número de página: ";
@@ -358,7 +459,8 @@ public class PokeAPI extends javax.swing.JFrame {
     
     public void consumoAPI() {
     String endpoint = "https://pokeapi.co/api/v2/pokemon";
-    try {
+    try 
+    {
         URL url = new URL(endpoint);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -369,6 +471,8 @@ public class PokeAPI extends javax.swing.JFrame {
             StringBuffer response = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
+    
+            
             }
             in.close();
 
@@ -432,6 +536,10 @@ public class PokeAPI extends javax.swing.JFrame {
                 }
                 in.close();
                 JsonObject data = JsonParser.parseString(response.toString()).getAsJsonObject();
+                contador_imagenes = 1;
+                etq_adelante.setEnabled(true);
+                etq_atras.setEnabled(false);
+                URL_actual_pokemon = data;
                 etq_nombre_pokemon.setText(data.get("name").getAsString().toUpperCase());
 
 
